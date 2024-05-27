a7275d491d0d796beb69d7fee43790ae4a4f6568
mohamed meqlad <mohamedmeqlad9@gmail.com>
2024-05-27 20:55:14 +0300

    Create config.py

---
+++ src/config/config.py
@@ -0,0 +1,18 @@
+onfig = {
+    "openai": {
+        "api_key": "OPENAI_KEY"
+    },
+    "kafka": {
+        "sasl.username": "KAFKA_CLUSTER_API_KEY",
+        "sasl.password": "KAFKA_CLUSTER_API_SECRET",
+        "bootstrap.servers": "KAFKA_CLUSTER_BOOTSTRAP_SERVER_URL:PORT",
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'session.timeout.ms': 4000
+    },
+    "schema_registry": {
+        "url": "SCHEMA_REGISTRY_URL",
+        "basic.auth.user.info": "SR_API_KEY:SR_API_SECRET"
+
+    }
+}
